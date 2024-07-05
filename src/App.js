6d349775fbe6eb18f4826f803a5613fdc1fda70d
Donata Deptula <dondeptu@gmail.com>
2024-07-05 19:56:41 +0200

    sample tasks moved to a seperate file

--- src/App.js
+++ src/App.js
@@ -3,21 +3,9 @@ import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import Section from "./Section";
 import Container from "./Container";
+import tasks from "./sampleTasks";
 
 let hideDoneTask = false;
-
-let tasks = [
-  {
-    id: 1,
-    content: "lekcja pianina",
-    done: true,
-  },
-  {
-    id: 2,
-    content: "spotkanie 16:30",
-    done: false,
-  },
-];
 
 function App() {
   return (
---
+++ src/sampleTasks.js
@@ -0,0 +1,14 @@
+const tasks = [
+    {
+        id: 1,
+        content: "lekcja pianina",
+        done: true,
+    },
+    {
+        id: 2,
+        content: "spotkanie 16:30",
+        done: false,
+    },
+];
+
+export default tasks;
