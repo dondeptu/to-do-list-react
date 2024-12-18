e1ec7c076eaba768ae27e859d0458bc66ecbad83
Donata Deptula <dondeptu@gmail.com>
2024-12-18 21:12:10 +0100

    add routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import Tasks from "./features/tasks/Tasks";
+
+import { HashRouter, NavLink, Route, Switch, Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import AuthorPage from "./features/author/AuthorPage";
 
 const App = () => (
-    <Tasks />
+    <HashRouter>
+        <nav>
+            <ul>
+                <li>
+                    <NavLink to="/zadania">Zadania</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/autor">O autorze</NavLink>
+                </li>
+            </ul>
+            <Switch>
+                <Route path="/zadania/:id">
+                    <TaskPage />
+                </Route>
+                <Route path="/zadania">
+                    <TasksPage />
+                </Route>
+                <Route path="/autor">
+                    <AuthorPage />
+                </Route>
+                <Route path="/">
+                    <Redirect to='/zadania' />
+                </Route>
+            </Switch>
+        </nav>
+    </HashRouter>
 );
 
 export default App;
